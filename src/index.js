e74914f7099e43c2ae66a61377768ceebeca1271
Javier Zapien <javierzapien@Javiers-MBP.hsd1.mi.comcast.net>
2021-08-18 11:08:50 -0400

    initial commit. with auth0, able to login and fetch user information from login. able to logout. very basic starting point. just setting up initial auth0 setup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='dev-u9j3svni.auth0.com'
+      clientId='1xKa6cYf5wZOGMa3tUwX2dSSLvJ5h77R'
+      redirectUri="http://localhost:3000"
+      >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
